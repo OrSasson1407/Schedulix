75dc290c005768f6ab920f49ea4bd8431936fcc9
OrSasson1407 <orsasson1407@gmail.com>
2026-06-09 16:23:07 +0300

    fix ci/cd

--- src/scheduler/BacktrackScheduler.py
+++ src/scheduler/BacktrackScheduler.py
@@ -304,6 +304,21 @@ def _queue_bridge(worker_args: dict, queue) -> None:
         queue.put(None)   # sentinel
 
 
+def _courses_conflict(course_a, course_b) -> bool:
+    a_map = {}
+    for prog in course_a.programs:
+        key = (prog.program_id, prog.year)
+        if key not in a_map or prog.requirement == Obligatory:
+            a_map[key] = prog.requirement
+
+    for prog in course_b.programs:
+        key = (prog.program_id, prog.year)
+        if key in a_map:
+            if a_map[key] == Obligatory or prog.requirement == Obligatory:
+                return True
+    return False
+
+
 class BacktrackScheduler(Scheduler):
     TIME_LIMIT_SECONDS = 28
 
